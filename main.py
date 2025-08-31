839074a76b05547e216fedcf8db14fb0659da36b
VolkanCARBUGA <volkanilkyar@gmail.com>
2025-08-31 16:25:22 +0300

    second commit

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {"data": {"name": "John Doe"}}
+@app.get("/blog")
+def index(limit: int, published: bool,sort):
+    if published:
+        return {"data": f"Blog list {limit} published blogs from the database"}
+    else:
+        return {"data": f"Blog list {limit} unpublished blogs from the database"}
+    
 
-@app.get("/about")
-def about():
-    return {"data": {"name": "John Doe", "age": 30}}
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data": f"Blog number {id}"}
+@app.get("/blog/unpublished")
+def unpublished():
+        return {"data": "all unpublished blogs"}
+@app.get("/blog/{id}/comments")
+def comments(id: int):
+    return {"data": ["Comment 1", "Comment 2"]}
+
+
 
